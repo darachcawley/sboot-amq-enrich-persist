1c773c7e402f374041daa17e489161ff44d593dc
darachcawley <darachcawley@gmail.com>
2019-12-05 17:14:54 +0000

    adding aggregator implementation to merge results from other rest and soap services

--- src/main/java/org/fuse/usecase/AccountAggregator.java
+++ src/main/java/org/fuse/usecase/AccountAggregator.java
@@ -19,6 +19,28 @@ public class AccountAggregator implements AggregationStrategy {
     @Override
     public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
 
+    	System.out.println("aggregating!!!");
+    	// the first endpoint being called by multicase will NOT have an oldExchange, 
+    	// only newExchange i.e. the rest call is first
+        if (oldExchange == null) {
+        	System.out.println("oldExchange is null");
+            return newExchange;
+        }
+
+        // catch any logging that might get called during the multicast
+        // for some (yet unknown) reason, you need a log entry within the multicast call in the route,
+        // otherwise it doesn't work - resulting in you needing this entry to catch log entries
+        if(oldExchange.getIn().getBody() instanceof String) {
+        	return newExchange;
+        }
+
+        // the second endpoint called is soap
+        Account acc = oldExchange.getIn().getBody(Account.class);
+        CorporateAccount ca = newExchange.getIn().getBody(CorporateAccount.class);
+
+        acc.setSalesRepresentative(ca.getSalesContact());
+        acc.setClientId(ca.getId());
+
         return oldExchange;
     }
     
